6472b826171de8661f8c5725af59018726cc0762
gunkol <gunnark13@gmail.com>
2018-02-15 17:55:02 +0000

    download csv

--- admin.js
+++ admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Client } = require('pg');
+const json2csv = require('json2csv');
 
 const connectionString = process.env.DATABASE_URL || 'postgres://:@localhost/v2';
 
@@ -24,12 +25,17 @@ async function fetchOrders() {
 
 router.get('/admin', ensureLoggedIn, async (req, res) => {
   const orders = await fetchOrders();
-  console.info(orders);
+
   res.render('admin', { orders });
 });
 
-router.get('/admin/download', ensureLoggedIn, (req, res) => {
-  res.send('<h1>Download</h1>');
+router.get('/admin/download', async (req, res) => {
+  const orders = await fetchOrders();
+  json2csv({ data: orders, quotes: '' }, (err, csv) => {
+    res.setHeader('Content-disposition', 'attachment; filename=orders.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(csv);
+  });
 });
 
 module.exports = router;
